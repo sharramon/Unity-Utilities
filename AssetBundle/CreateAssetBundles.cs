1c8624d6e2adf3a63aa34b3d9014773780c58cb6
sharramon <61921450+sharramon@users.noreply.github.com>
2023-02-15 23:44:36 +0900

    Set asset bundle name as folder name

--- AssetBundle/CreateAssetBundles.cs
+++ AssetBundle/CreateAssetBundles.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using UnityEditor;
 using UnityEngine;
+using System.IO;
 
 public class CreateAssetBundles
 {
-    [MenuItem("Assets/Create Asset Bundles")]
-    private static void BuildAllAssetBundles(string assetBundleDirectoryPath = null)
+    [MenuItem("AssetBundles/Create Asset Bundles")]
+    private static void BuildAllAssetBundles()
     {
-        if(assetBundleDirectoryPath == null)
-            assetBundleDirectoryPath = Application.dataPath + "/../AssetBundles";
+        string assetBundleDirectoryPath = Application.dataPath + "/../AssetBundles";
+
         try
         {
             BuildPipeline.BuildAssetBundles(assetBundleDirectoryPath, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
@@ -16,6 +17,34 @@ public class CreateAssetBundles
         catch(Exception e)
         {
             Debug.LogWarning(e);
+        }
+    }
+
+    [MenuItem("AssetBundles/Set Asset Bundle From File Name")]
+    private static void SetAssetBundlesFromFileNames()
+    {
+        if (Selection.assetGUIDs.Length > 0)
+        {
+            Debug.Log($"bundles started with length of {Selection.assetGUIDs.Length}");
+            Debug.Log($"bundles objects length is {Selection.objects.Length}");
+            foreach (UnityEngine.Object asset in Selection.objects)
+            {
+                //get the asset path
+                string assetPath = AssetDatabase.GetAssetPath(asset);
+                Debug.Log($"path is {assetPath}");
+                AssetImporter assetImporter = AssetImporter.GetAtPath(assetPath);
+
+                //get name of folder of asset and set that to the name of the bundle
+                string folderName = Path.GetFileName(Path.GetDirectoryName(assetPath));
+                Debug.Log($"directory name is {Path.GetDirectoryName(assetPath)}");
+                Debug.Log($"Asset bundle name is {folderName}");
+                assetImporter.assetBundleName = folderName;
+                Debug.Log(Selection.assetGUIDs.Length + " Asset Bundles Assigned");
+            }
+        }
+        else
+        {
+            Debug.Log("No Assets Selected");
         }
     }
 }
--- AssetBundle/ReadMe.txt
+++ AssetBundle/ReadMe.txt
@@ -1,4 +1,5 @@
 Just code from this youtube series :
 https://www.youtube.com/watch?v=eSCrZwairVw&t=526s
 
-It was a very clean way of using asset bundles
+It was a very clean way of using asset bundles. Requires Api comptibility level to be .NET Framework under Project Settings => player
+due to the dynamic keyword
